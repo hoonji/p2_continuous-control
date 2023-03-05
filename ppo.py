f40e95cc09a6025322bc4af8898ec0a76e7461f7
hoonji <hoonji126@gmail.com>
2023-03-05 18:30:58 +0900

    refactor init
    LEARNING_RATE = 3e-4
    ADAM_EPS = 1e-5
    TOTAL_TIMESTEPS = 2000000
    GAMMA = .99
    LAMBDA = .95
    UPDATE_EPOCHS = 10
    N_MINIBATCHES = 32
    CLIP_COEF = .2
    MAX_GRAD_NORM = 5
    GAE_LAMBDA = .95
    V_COEF = .5
    HIDDEN_LAYER_SIZE = 512
    ROLLOUT_LEN = 2048

--- ppo.py
+++ ppo.py
@@ -1,51 +1,20 @@
-# MIT License
-
-# Copyright (c) 2022 Shengyi Huang, Rousslan Fernand Julien Dossa, Antonin Raffin,
-                   # Anssi Kanervisto, Weixun Wang
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# Modified by Ji Hoon (Hoonji) Baek
-# - modified to support unityagents multi agent Reacher environment
-# - hyperparameter tuning
-# - updated network architecture
-# - removed non-essential code
-# - simplified score recording
-# - checkpoints model
+# Implementation based on Costa Huang's PPO implementation: https://github.com/vwxyzjn/ppo-implementation-details/blob/main/ppo_continuous_action.py
 
 import time
 import pickle
-import math
 import random
-from collections import deque
+from collections import namedtuple
 
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.distributions.normal import Normal
 
 from ppo_agent import Agent
 
 LEARNING_RATE = 3e-4
 ADAM_EPS = 1e-5
-TOTAL_TIMESTEPS = 4000000
+TOTAL_TIMESTEPS = 2000000
 GAMMA = .99
 LAMBDA = .95
 UPDATE_EPOCHS = 10
@@ -54,10 +23,14 @@ CLIP_COEF = .2
 MAX_GRAD_NORM = 5
 GAE_LAMBDA = .95
 V_COEF = .5
-ENT_COEF = .01
 HIDDEN_LAYER_SIZE = 512
-ANNEAL_LR = False
 ROLLOUT_LEN = 2048
+
+Rollout = namedtuple(
+    'Rollout',
+    ['observations', 'actions', 'logprobs', 'rewards', 'dones', 'values'])
+Batch = namedtuple(
+    'Batch', ['observations', 'actions', 'advantages', 'returns', 'logprobs'])
 
 
 def run_ppo(env):
@@ -77,16 +50,17 @@ def run_ppo(env):
   agent = Agent(n_observations, n_actions, HIDDEN_LAYER_SIZE).to(device)
   optimizer = optim.Adam(agent.parameters(), lr=LEARNING_RATE, eps=ADAM_EPS)
 
-  obs = torch.zeros((ROLLOUT_LEN, num_agents, n_observations)).to(device)
-  actions = torch.zeros((ROLLOUT_LEN, num_agents, n_actions)).to(device)
-  logprobs = torch.zeros(ROLLOUT_LEN, num_agents).to(device)
-  rewards = torch.zeros(ROLLOUT_LEN, num_agents).to(device)
-  advantages = torch.zeros(ROLLOUT_LEN, num_agents).to(device)
-  dones = torch.zeros(ROLLOUT_LEN, num_agents).to(device)
-  values = torch.zeros(ROLLOUT_LEN, num_agents).to(device)
+  rollout = Rollout(observations=torch.zeros(
+      (ROLLOUT_LEN, num_agents, n_observations)).to(device),
+                    actions=torch.zeros(
+                        (ROLLOUT_LEN, num_agents, n_actions)).to(device),
+                    logprobs=torch.zeros(ROLLOUT_LEN, num_agents).to(device),
+                    rewards=torch.zeros(ROLLOUT_LEN, num_agents).to(device),
+                    dones=torch.zeros(ROLLOUT_LEN, num_agents).to(device),
+                    values=torch.zeros(ROLLOUT_LEN, num_agents).to(device))
 
-  next_obs = torch.Tensor(env_info.vector_observations).to(device)
-  next_done = torch.zeros(num_agents).to(device)
+  next_observations = torch.Tensor(env_info.vector_observations).to(device)
+  next_dones = torch.zeros(num_agents).to(device)
   num_updates = TOTAL_TIMESTEPS // batch_size
   minibatch_size = batch_size // N_MINIBATCHES
   current_returns = np.zeros(num_agents)
@@ -98,100 +72,87 @@ def run_ppo(env):
         f"update {update}/{num_updates}. Last update in {time.time() - time_checkpoint}s"
     )
     time_checkpoint = time.time()
-    # Anneal learning rate
-    if ANNEAL_LR:
-      frac = 1.0 - (update - 1.0) / num_updates
-      optimizer.param_groups[0]["lr"] = frac * LEARNING_RATE
 
     for step in range(ROLLOUT_LEN):
-      obs[step] = next_obs
-      dones[step] = next_done
+      observations = next_observations
 
-      # import pdb; pdb.set_trace()
       with torch.no_grad():
-        cur_actions, cur_logprobs, _, cur_values = agent.get_action_and_value(next_obs)
-      values[step] = cur_values.flatten()
-      actions[step] = cur_actions
-      logprobs[step] = cur_logprobs
+        actions, logprobs = agent.get_actions_and_logprobs(
+            observations)
+        values = agent.predict_values(observations)
+      env_info = env.step(actions.cpu().numpy())[brain_name]
+      dones = env_info.local_done
 
-      env_info = env.step(cur_actions.cpu().numpy())[brain_name]
-      rewards[step] = torch.tensor(env_info.rewards).to(device)
+      rollout.observations[step] = observations
+      rollout.actions[step] = actions
+      rollout.rewards[step] = torch.tensor(env_info.rewards).to(device)
+      rollout.dones[step] = next_dones  # record previous dones for this step
+      rollout.logprobs[step] = logprobs
+      rollout.values[step] = values.flatten()
 
+      # Record agent returns
       current_returns += env_info.rewards
-      scores.extend(current_returns[env_info.local_done])
-      current_returns[env_info.local_done] = 0
+      scores.extend(current_returns[dones])
+      current_returns[dones] = 0
 
-      next_obs = torch.Tensor(env_info.vector_observations).to(device)
-      next_done = torch.Tensor([env_info.local_done]).to(device)
+      next_observations = torch.Tensor(env_info.vector_observations).to(device)
+      next_dones = torch.Tensor([dones]).to(device)
 
-    with torch.no_grad():
-      next_value = agent.get_value(next_obs).flatten()
-
-    lastgaelam = 0
+    advantages = torch.zeros(ROLLOUT_LEN, num_agents).to(device)
+    z = 0
     for t in reversed(range(ROLLOUT_LEN)):
       if t == ROLLOUT_LEN - 1:
-        nextnonterminal = 1.0 - next_done
-        nextvalues = next_value
+        next_nonterminal = 1.0 - next_dones
+        with torch.no_grad():
+          next_values = agent.predict_values(next_observations).flatten()
       else:
-        nextnonterminal = 1.0 - dones[t + 1]
-        nextvalues = values[t + 1]
-      delta = rewards[t] + GAMMA * nextvalues * nextnonterminal - values[t]
-      advantages[
-          t] = lastgaelam = delta + GAMMA * GAE_LAMBDA * nextnonterminal * lastgaelam
-    returns = advantages + values
+        next_nonterminal = 1.0 - rollout.dones[t + 1]
+        next_values = rollout.values[t + 1]
+      td_errors = rollout.rewards[
+          t] + next_nonterminal * GAMMA * next_values - rollout.values[t]
+      z = td_errors + next_nonterminal * GAMMA * GAE_LAMBDA * z
+      advantages[t] = z
 
-    b_obs = obs.reshape((-1, n_observations))
-    b_actions = actions.reshape((-1, n_actions))
-    b_logprobs = logprobs.reshape(-1)
-    b_advantages = advantages.reshape(-1)
-    b_returns = returns.reshape(-1)
-    b_values = values.reshape(-1)
+    # Reshape rollout variables for training
+    batch = Batch(observations=rollout.observations.reshape(
+        (-1, n_observations)),
+                  actions=rollout.actions.reshape((-1, n_actions)),
+                  advantages=advantages.reshape(-1),
+                  returns=(advantages + rollout.values).reshape(-1),
+                  logprobs=rollout.logprobs.reshape(-1))
 
-    b_inds = np.arange(batch_size)
+    batch_indices = np.arange(batch_size)
     for epoch in range(UPDATE_EPOCHS):
-      np.random.shuffle(b_inds)
+      np.random.shuffle(batch_indices)
       for start in range(0, batch_size, minibatch_size):
-        end = start + minibatch_size
-        mb_inds = b_inds[start:end]
+        mbatch_indices = batch_indices[start:start + minibatch_size]
+        minibatch = Batch(observations=batch.observations[mbatch_indices],
+                          actions=batch.actions[mbatch_indices],
+                          advantages=batch.advantages[mbatch_indices],
+                          returns=batch.advantages[mbatch_indices],
+                          logprobs=batch.advantages[mbatch_indices])
 
-        _, newlogprob, entropy, newvalue = agent.get_action_and_value(
-            b_obs[mb_inds], b_actions[mb_inds])
-        logratio = newlogprob - b_logprobs[mb_inds]
-        ratio = logratio.exp()
+        _, logprobs = agent.get_actions_and_logprobs(minibatch.observations,
+                                                    minibatch.actions)
+        values = agent.predict_values(minibatch.observations)
+        logratios = logprobs - minibatch.logprobs
+        ratios = logratios.exp()
 
-        mb_advantages = b_advantages[mb_inds]
-        # Advantage normalization
-        mb_advantages = (mb_advantages -
-                         mb_advantages.mean()) / (mb_advantages.std() + 1e-8)
-
-        # Policy loss
-        pg_loss1 = -mb_advantages * ratio
-        pg_loss2 = -mb_advantages * torch.clamp(ratio, 1 - CLIP_COEF,
-                                                1 + CLIP_COEF)
+        # Surrogate objective
+        pg_loss1 = -minibatch.advantages * ratios
+        pg_loss2 = -minibatch.advantages * torch.clamp(ratios, 1 - CLIP_COEF,
+                                                       1 + CLIP_COEF)
         pg_loss = torch.max(pg_loss1, pg_loss2).mean()
-
-        # Value loss
-        newvalue = newvalue.view(-1)
-        v_loss_unclipped = (newvalue - b_returns[mb_inds])**2
-        v_clipped = b_values[mb_inds] + torch.clamp(
-            newvalue - b_values[mb_inds],
-            -CLIP_COEF,
-            CLIP_COEF,
-        )
-        v_loss_clipped = (v_clipped - b_returns[mb_inds])**2
-        v_loss_max = torch.max(v_loss_unclipped, v_loss_clipped)
-        v_loss = v_loss_max.mean()
-
-        entropy_loss = entropy.mean()
-        loss = pg_loss + V_COEF * v_loss - ENT_COEF * entropy_loss
+        value_loss = ((values - minibatch.returns)**2).mean()
+        loss = pg_loss + V_COEF * value_loss
 
         optimizer.zero_grad()
         loss.backward()
         nn.utils.clip_grad_norm_(agent.parameters(), MAX_GRAD_NORM)
         optimizer.step()
 
-    torch.save(agent.state_dict(), f'checkpoints/model_checkpoint.pickle')
-    with open(f'checkpoints/scores.pickle', 'wb') as f:
+    torch.save(agent.state_dict(), f'model_checkpoint.pickle')
+    with open(f'scores.pickle', 'wb') as f:
       pickle.dump(scores, f)
 
     print(f'last 100 returns: {np.array(scores[-100:]).mean()}')
--- ppo_agent.py
+++ ppo_agent.py
@@ -1,77 +1,35 @@
-# MIT License
-
-# Copyright (c) 2022 Shengyi Huang, Rousslan Fernand Julien Dossa, Antonin Raffin,
-                   # Anssi Kanervisto, Weixun Wang
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# Modified by Ji Hoon (Hoonji) Baek
-# - updated network architecture
-
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.optim as optim
 from torch.distributions.normal import Normal
-import random
 
-def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
-  torch.nn.init.orthogonal_(layer.weight, std)
-  torch.nn.init.constant_(layer.bias, bias_const)
-  return layer
 
 class Agent(nn.Module):
 
   def __init__(self, n_observations, n_actions, hidden_size=256):
     super().__init__()
     self.critic = nn.Sequential(
-        layer_init(
-            nn.Linear(
-                np.array(n_observations).prod(), hidden_size)),
+        nn.Linear(n_observations, hidden_size),
         nn.ReLU(),
-        layer_init(nn.Linear(hidden_size, hidden_size)),
+        nn.Linear(hidden_size, hidden_size),
         nn.ReLU(),
-        layer_init(nn.Linear(hidden_size, 1), std=1.0),
+        nn.Linear(hidden_size, 1),
         nn.Tanh(),
     )
-    self.actor_mean = nn.Sequential(
-        layer_init(
-            nn.Linear(
-                np.array(n_observations).prod(), hidden_size)),
-        nn.ReLU(),
-        layer_init(nn.Linear(hidden_size, hidden_size)),
-        nn.ReLU(),
-        layer_init(nn.Linear(hidden_size, np.prod(n_actions)),
-                   std=0.01),
-    )
-    self.actor_logstd = nn.Parameter(
-        torch.zeros(1, np.prod(n_actions)))
+    self.actor_means = nn.Sequential(nn.Linear(n_observations, hidden_size),
+                                     nn.ReLU(),
+                                     nn.Linear(hidden_size, hidden_size),
+                                     nn.ReLU(),
+                                     nn.Linear(hidden_size, n_actions))
+    self.actor_logstd = nn.Parameter(torch.zeros(1, n_actions))
 
-  def get_value(self, x):
+  def predict_values(self, x):
     return self.critic(x)
 
-  def get_action_and_value(self, x, action=None):
-    action_mean = self.actor_mean(x)
-    action_logstd = self.actor_logstd.expand_as(action_mean)
-    action_std = torch.exp(action_logstd)
-    probs = Normal(action_mean, action_std)
+  def get_actions_and_logprobs(self, x, action=None):
+    action_means = self.actor_means(x)
+    action_stddevs = torch.exp(self.actor_logstd.expand_as(action_means))
+    probs = Normal(action_means, action_stddevs)
     if action is None:
       action = probs.sample()
-    return action, probs.log_prob(action).sum(1), probs.entropy().sum(
-        1), self.critic(x)
+    return action, probs.log_prob(action).sum(1)
